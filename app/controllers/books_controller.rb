986c64437fbce3c611ac9a902c3c94ef90a192d1
Fanny <venezia.fanny@gmail.com>
2023-07-19 17:38:59 +0200

    Creation des fonctions du controller et des views

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,43 @@
 class BooksController < ApplicationController
+  before_action :set_book, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @books = Book.all
+  end
+
+  def show
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.save
+    redirect_to book_path(@book)
+  end
+
+  def edit
+  end
+
+  def update
+    @book.update(book_params)
+    redirect_to book_path(@book)
+  end
+
+  def destroy
+    @book.destroy
+    redirect_to restaurants_path, status: :see_other
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :author, :description)
+  end
+
+  def set_book
+    @book = Book.find(params[:id])
+  end
 end
